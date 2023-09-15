003d2d6372b26d5d3a04b7c542a39614b20fa72c
sazzad46031 <git config --global user.email sazzadmgbs@gmail.com>
2023-09-15 23:07:32 +0600

    Toast have been added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,22 +3,33 @@ import { useState } from 'react';
 import './Home.css'
 import { useEffect } from 'react';
 import Cart from '../Cart/Cart';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const Home = () => {
     const [allCourse, setAllCourse] = useState([])
     const [selectedCourse, setSelectedCourse] = useState([])
     const [totalCredit, setTotalCredit] = useState(0)
     const [remaining,setRemaining] = useState(0)
-    
+    const notify = () => toast.error("Already booked!",{
+        position : 'top-center'
+    });
+    const notify2 = () => toast.error("Credit should less than 20 hour",{
+        position : 'top-center'
+    });
     useEffect(()=>{
         fetch('json.data')
         .then(res => res.json())
         .then(data=> setAllCourse(data))
     },[])
+
+   
+    
     const handleSelectCourse = course =>{
         const isExist = selectedCourse.find(item => item.id === course.id)
+        console.log(isExist)
         let count = course.credit;
         if(isExist){
-            return alert('this does not work')
+            notify();
         }
         else{
             selectedCourse.forEach((item) =>{
@@ -26,7 +37,7 @@ const Home = () => {
             })
             const totalRemaining = 20 - count;
             if(count > 20){
-                return alert('it does not work')
+                notify2();
             }
             else{
                 setRemaining(totalRemaining)
@@ -39,11 +50,10 @@ const Home = () => {
     }
     
     return (
-       <div>
+       <><div>
             <div className='home-container'>
                 <div className='card-container'>
-                    {
-                        allCourse.map(course =>(<div key={course.id} className='card'>
+                    {allCourse.map(course => (<div key={course.id} className='card'>
                         <img src={course.img} alt="" />
                         <h3 className='title'>{course.title}</h3>
                         <p>{course.details}</p>
@@ -51,15 +61,18 @@ const Home = () => {
                             <p className='price'><i className="fa-solid fa-dollar-sign icon"></i>Price :  {course.price}</p>
                             <p><i className="fa-solid fa-book-open icon"></i>credit: {course.credit} hr</p>
                         </div>
-                        <button onClick={()=>handleSelectCourse(course)} className='btn'>Select</button>
-                    </div>))
-                    }
+                        <div>
+                            <button onClick={ () =>  handleSelectCourse(course) } className='btn'>Select</button>
+                        </div>
+                        
+                    </div>))}
                 </div>
                 <div className='cart'>
                     <Cart selectedCourse={selectedCourse} remaining={remaining} totalCredit={totalCredit}></Cart>
                 </div>
             </div>
-       </div>
+        </div>
+        <ToastContainer></ToastContainer></>
     );
 };
 
